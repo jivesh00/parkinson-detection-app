e9276984be0f014d3043ae8911f59d5aaf6ee8e0
jivesh00 <omjees48@gmail.com>
2026-04-07 16:38:38 +0530

    final

--- app.py
+++ app.py
@@ -205,5 +205,7 @@ def logout():
     return redirect("/")
 
 # ---------------- RUN ----------------
+import os
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
